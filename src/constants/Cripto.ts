79d57f267f49c93163e9ba0707ff5207e9c4f69d
Alejo <alejandro.forero@miaguila.com>
2021-05-02 22:47:32 -0500

    feat(const): add cripto constants

---
+++ src/constants/Cripto.ts
@@ -0,0 +1,8 @@
+import { Option } from '../components/Selector/Selector'
+
+export const FilterOptions: Array<Option> = [
+  { label: 'Name', value: 'name' },
+  { label: 'Price', value: 'price' },
+  { label: 'Market Cap', value: 'marketCap' },
+  { label: 'Circulating Supply', value: 'circulatingSupply' },
+];
