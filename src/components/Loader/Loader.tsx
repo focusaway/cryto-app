cdfdc2fa176e8ecf15566d7d037c7b16341cb51f
Alejo <alejandro.forero@miaguila.com>
2021-05-02 22:39:01 -0500

    feat(ui): create loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import './Loader.scss';
+const Loader: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="Loader rounded-full w-16 h-16 border-4" />
+    </div>
+  );
+};
+
+export default Loader;
