f0b924078fc2a9eadb76e31caca375ec2bd51485
Alejo <alejandro.forero@miaguila.com>
2021-05-04 02:14:23 -0500

    docs(input): add input docs

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,7 +8,16 @@ export interface Props {
   type?: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
-
+/**
+ * Component for standard input
+ * @component
+ * @prop   {string} classes  Classes css
+ * @prop   {string} name  Name HTML attribute
+ * @prop   {string}  label Label attribute
+ * @prop   {string} value  Value of data input
+ * @prop   {string} type  Type HTML attribute
+ * @prop   {function} onChange  Callback when value is changed
+ */
 const Input: React.FC<Props> = ({ label, name, classes, value, type, onChange }) => {
   const inputClasses=`${classes} rounded-3xl`
   const handleInput = (event: ChangeEvent<HTMLInputElement>): void => {
