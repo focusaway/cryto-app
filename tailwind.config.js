17ed82e5a0a2c87cd47aba61c447a53661ba35b3
Alejo <alejandro.forero@miaguila.com>
2021-05-04 03:05:15 -0500

    fix(tailwind): add safelist for dynamic class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      safelist: {
+        standard: [/grid-cols-\d/],
+      }
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
